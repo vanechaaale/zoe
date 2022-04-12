14a5b4cc453c8f1f7fc6d0230491b63e907dd234
Vanessa <vanessal2001@gmail.com>
2022-04-12 17:24:57 -0400

    adding tokens file

--- constants.py
+++ constants.py
@@ -24,7 +24,9 @@ API = Lolesports_API()
 WATCHER = LolWatcher(API_KEY)
 my_region = 'na1'
 FREE_CHAMPION_IDS = WATCHER.champion.rotations(my_region)
-db = TinyDB('Data/test_database.json')
+
+db = TinyDB('Data/database.json')
+# db = TinyDB('Data/test_database.json')
 
 # check league's latest version
 latest = WATCHER.data_dragon.versions_for_region(my_region)['n']['champion']
