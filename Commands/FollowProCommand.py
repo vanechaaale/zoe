3b47204bcf93d99961ae3aa96cb2c466b6b4c252
Vanessa <vanessal2001@gmail.com>
2022-07-23 18:42:46 -0400

    add followprocommand and followskincommand files to commands

---
+++ Commands/FollowProCommand.py
@@ -0,0 +1,49 @@
+import asyncio
+import discord
+from tinydb import Query
+
+
+async def follow(message, base_command, *champion_name):
+    # format champion_name
+    champion_name = base_command.format_champion_name(' '.join(champion_name))
+    if not champion_name:
+        await message.channel.send(
+            f"use '~follow <champion>' to be notified when a champion is being played in a professional match!")
+        return
+    # Query champion user id list
+    champion = Query()
+    db = base_command.db
+    user_id = message.author.id
+    champ_name_user_ids_dict = db.get(champion['champion_name'] == champion_name)
+    # I tried using a set but it broke whenever i called db.insert()
+    user_ids_list = [] if champ_name_user_ids_dict is None else champ_name_user_ids_dict['user_ids']
+    if champ_name_user_ids_dict is None:
+        user_ids_list.append(user_id)
+        db.insert({'champion_name': champion_name, 'user_ids': user_ids_list})
+        await message.channel.send(f"Now tracking live professional games for {champion_name}.")
+    elif user_id not in user_ids_list:
+        user_ids_list.append(user_id)
+        db.update({'user_ids': user_ids_list}, champion['champion_name'] == champion_name)
+        await message.channel.send(f"Now following live professional games for {champion_name}.")
+    else:
+        user_ids_list.remove(user_id)
+        db.update({'user_ids': user_ids_list}, champion['champion_name'] == champion_name)
+        await message.channel.send(f"No longer following live professional games for {champion_name}.")
+
+
+async def following(message, base_command):
+    tracked_list = []
+    user_id = message.author.id
+    for champ_name in base_command.champ_dict.values():
+        champion = Query()
+        db = base_command.db
+        query_results = db.get(champion['champion_name'] == champ_name)
+        if query_results is not None:
+            user_ids_list = query_results['user_ids']
+            if user_id in user_ids_list:
+                tracked_list.append(champ_name)
+    if len(tracked_list) != 0:
+        await message.channel.send(
+            f"Currently tracking professional matches for: {', '.join(tracked_list)}")
+    else:
+        await message.channel.send(f"You are currently not tracking live games for any champion.")
--- zoe_bot.py
+++ zoe_bot.py
@@ -1,4 +1,5 @@
 import BaseMessageResponse
+import Commands
 import Data
 import PIL
 import asyncio
@@ -13,11 +14,10 @@ import requests
 import shutil
 import threading
 import time
-from constants import *
-import Commands
-from Commands import GuideCommand, LiveCommand, SaleCommand
+from Commands import GuideCommand, LiveCommand, SaleCommand, FollowProCommand
 from Data import Quotes, gifs
 from PIL import Image
+from constants import *
 from discord import Webhook, RequestsWebhookAdapter, File
 from discord.ext import commands
 from discord.ext.commands import Context
@@ -28,255 +28,6 @@ from riotwatcher import LolWatcher
 from threading import Thread
 from tinydb import TinyDB, Query, where
 from urllib.request import Request, urlopen
-
-
-# base_cmd = BaseCommand()
-# bot = base_cmd.bot
-
-#
-# # BaseCommands
-# base_commands = BaseCommand()
-# bot = base_commands.bot
-# api = base_commands.api
-# CHAMP_DICT = base_commands.champ_dict
-# db = base_commands.db
-# SKIN_DB = base_commands.skin_db
-# free_champion_ids = base_commands.free_champ_ids
-#
-#
-# @bot.event
-# async def on_ready():
-#     cache_clear_hours = 2
-#     check_tracked_mins = 3 * 60
-#     activity = discord.Game(name="Do something fun! The world might be ending... Or not!")
-#     await bot.change_presence(status=discord.Status.online, activity=activity)
-#     while True:
-#         await check_tracked_champions()
-#         await clear_cache(cache_clear_hours)
-#         await asyncio.sleep(check_tracked_mins)
-#
-#
-# @bot.command(hidden=True)
-# @commands.is_owner()
-# async def shutdown(c):
-#     await c.channel.send("Logging out...")
-#     await bot.close()
-#
-#
-# @bot.event
-# async def on_guild_join(guild):
-#     for channel in guild.text_channels:
-#         if channel.permissions_for(guild.me).send_messages:
-#             quote = random.choice(Quotes.Zoe_join_server_message)
-#             await channel.send(f'"*{quote}*"')
-#         break
-#
-#
-# @bot.event
-# async def on_command_error(channel, error):
-#     if isinstance(error, commands.CommandNotFound):
-#         return
-#     else:
-#         await channel.send(error)
-#
-#
-# @bot.listen()
-# async def on_message(message):
-#     if message.author == bot.user:
-#         return
-#     for cmd in BaseMessageResponse.RESPONSES:
-#         if cmd.invoke(message) and not const.is_command(message):
-#             await cmd.execute(message)
-#     if message.is_system() and message.type == discord.MessageType.new_member:
-#         quote = random.choice(Quotes.Greet)
-#         await message.channel.send(f'"*{quote}*"')
-#
-#
-# @bot.command(brief="Show Zoe guides", description="List of Zoe guides and Zoe players!")
-# async def guide(channel):
-#     await channel.send("Here are some guides to the Aspect of Twilight and some streamers who play her!\n\n")
-#     detention = discord.Embed(
-#         color=0xffb6c1,
-#     )
-#     detention.add_field(name="Detention", value="NA Challenger Mid Laner", inline=False)
-#     detention.set_thumbnail(url='https://pbs.twimg.com/profile_images/1465745862940897281/Q_QU3wNS_400x400.png')
-#     detention.add_field(name="Youtube Guide", value='https://www.youtube.com/watch?v=YW37o9TVTho')
-#     await channel.send(embed=detention)
-#     # vicksy
-#     vicksy = discord.Embed(
-#         color=0xffb6c1)
-#     vicksy.add_field(name="Vicksy", value="EUW Master Mid Laner", inline=False)
-#     vicksy.add_field(
-#         name="Mobafire Guide",
-#         value="https://www.mobafire.com/league-of-legends/build/vicksys-updated-master-zoe-guide-for-season-12"
-#               "-524904",
-#         inline=False)
-#     vicksy.set_thumbnail(
-#         url="https://static-cdn.jtvnw.net/jtv_user_pictures/37e7e61e-369f-4ba6-8fd6-106f860eca82-profile_image"
-#             "-70x70.png")
-#     await channel.send(embed=vicksy)
-#     # pekin woof
-#     pekin = discord.Embed(color=0xffb6c1, url="https://www.youtube.com/user/PekinGaming")
-#     pekin.add_field(
-#         name="Pekin Woof",
-#         value="Ex-Pro Player, NA Challenger Mid Laner",
-#         inline=False)
-#     pekin.add_field(
-#         name="Youtube Channel", value="https://www.youtube.com/user/PekinGaming")
-#     pekin.set_thumbnail(
-#         url="https://yt3.ggpht.com/ytc/AKedOLTPLJMSw3i8BAqEGeZjEbYlMPlcYwF8Ted417Omew=s88-c-k-c0x00ffffff-no-rj")
-#     await channel.send(embed=pekin)
-#
-#
-# @bot.command(brief="Zoe gifs.", description="Beautiful Zoe gifs.")
-# async def gif(channel):
-#     await channel.send(random.choice(gifs.gifs))
-#
-#
-# @bot.command(brief="Show Zoe matchup tips! (WIP)", description="View Zoe's matchup statistics against a champion")
-# async def matchup(channel, champion):
-#     champion.lower()
-#     await channel.send(const.get_zoe_error_message())
-#
-#
-# @bot.command(brief="Paddle Star Damage Calculator (WIP)",
-#              description="Zoe Q damage calculator based on items, runes, and masteries")
-# async def psdc(channel):
-#     await channel.send(const.get_zoe_error_message())
-#
-#
-# @bot.command(brief="Show the weekly free champion rotation",
-#              description="Weekly free to play champion rotation for summoner level 11+ accounts")
-# async def rotation(c):
-#     try:
-#         free_rotation = []
-#         for champion_id in free_champion_ids['freeChampionIds']:
-#             free_rotation.append(CHAMP_DICT[str(champion_id)])
-#         free_rotation.sort()
-#         free_rotation = ', '.join(free_rotation)
-#         await c.channel.send("The champions in this week's free to play rotation are: " + free_rotation)
-#     except (Exception,):
-#         await c.channel.send(const.get_zoe_error_message())
-#
-#
-# @matchup.error
-# async def matchup_error(ctx, error):
-#     if isinstance(error, discord.ext.commands.errors.MissingRequiredArgument):
-#         await ctx.send("use '~matchup <champion>' to search for Zoe's matchup statistics against a champion!")
-#
-#
-# @bot.command(brief="Show champion skins on sale this week",
-#              description="Show list of all champion skins on sale, which refreshes every Monday at 3 pm EST")
-# async def sale(channel):
-#     sale_skins_name_rp_costs = []
-#     embed = discord.Embed(title="Champion skins sale:",
-#                           description="\n", color=0x87cefa)
-#     try:
-#         with open("Data/skin_sales_data.json", 'r') as file:
-#             dictionary = json.load(file)
-#         # iterate through dictionary and get list of skins on sale
-#         for entry in dictionary:
-#             skin_name_rp_cost = " ".join(entry['skin_name_rp_cost'].split())
-#             skin_data = skin_name_rp_cost.split(' ')
-#             skin_name = skin_data[0: len(skin_data) - 3]
-#             skin_rp_cost = skin_data[len(skin_data) - 2: len(skin_data)]
-#             embed.add_field(
-#                 name=f"{' '.join(skin_name)}",
-#                 value=' '.join(skin_rp_cost),
-#                 inline=True)
-#             sale_skins_name_rp_costs.append(skin_name_rp_cost)
-#         # skins_sale = '\n'.join(sale_skins_name_rp_costs)
-#         # await c.channel.send("List of champion skins on sale this week: \n" + skins_sale)
-#         image_file = discord.File('Data/full_skin_sales_image.jpg', filename='full_skin_sales_image.jpg')
-#         embed.set_image(url='attachment://full_skin_sales_image.jpg')
-#         await channel.send(embed=embed, file=image_file)
-#     except (Exception,):
-#         await channel.channel.send(random.choice(Quotes.Zoe_error_message))
-#
-#
-# @bot.command()
-# async def test(message, *champion_name):
-#     # format champion_name
-#     champion_name = const.format_champion_name(' '.join(champion_name))
-#     if not champion_name:
-#         await message.channel.send(
-#             f"use '~test <champion>' to be notified when a champion's skins go on sale!")
-#         return
-#     # Query champion user id list
-#     champion = Query()
-#     user_id = message.author.id
-#     skin_db = SKIN_DB
-#     champ_name_user_ids_dict = skin_db.get(champion['champion_name'] == champion_name)
-#     # I tried using a set but it broke whenever i called db.insert()
-#     user_ids_list = [] if champ_name_user_ids_dict is None else champ_name_user_ids_dict['user_ids']
-#     if champ_name_user_ids_dict is None:
-#         user_ids_list.append(user_id)
-#         skin_db.insert({'champion_name': champion_name, 'user_ids': user_ids_list})
-#         await message.channel.send(f"Now tracking skin sales for {champion_name}.")
-#     elif user_id not in user_ids_list:
-#         user_ids_list.append(user_id)
-#         skin_db.update({'user_ids': user_ids_list}, champion['champion_name'] == champion_name)
-#         await message.channel.send(f"Now tracking skin sales for {champion_name}.")
-#     else:
-#         user_ids_list.remove(user_id)
-#         skin_db.update({'user_ids': user_ids_list}, champion['champion_name'] == champion_name)
-#         await message.channel.send(f"No longer tracking skin sales for {champion_name}.")
-#
-#
-# @bot.command(brief="Track a champion in professional play",
-#              description="Receive messages from Zoe Bot whenever the given champion is being played in a professional "
-#                          "game, or use the command again to stop receiving notifications from Zoe Bot.")
-# async def follow(message, *champion_name):
-#     # format champion_name
-#     champion_name = const.format_champion_name(' '.join(champion_name))
-#     if not champion_name:
-#         await message.channel.send(
-#             f"use '~follow <champion>' to be notified when a champion is being played in a professional match!")
-#         return
-#     # Query champion user id list
-#     champion = Query()
-#     user_id = message.author.id
-#     champ_name_user_ids_dict = db.get(champion['champion_name'] == champion_name)
-#     # I tried using a set but it broke whenever i called db.insert()
-#     user_ids_list = [] if champ_name_user_ids_dict is None else champ_name_user_ids_dict['user_ids']
-#     if champ_name_user_ids_dict is None:
-#         user_ids_list.append(user_id)
-#         db.insert({'champion_name': champion_name, 'user_ids': user_ids_list})
-#         await message.channel.send(f"Now tracking live professional games for {champion_name}.")
-#     elif user_id not in user_ids_list:
-#         user_ids_list.append(user_id)
-#         db.update({'user_ids': user_ids_list}, champion['champion_name'] == champion_name)
-#         await message.channel.send(f"Now following live professional games for {champion_name}.")
-#     else:
-#         user_ids_list.remove(user_id)
-#         db.update({'user_ids': user_ids_list}, champion['champion_name'] == champion_name)
-#         await message.channel.send(f"No longer following live professional games for {champion_name}.")
-#
-#
-# @bot.command(brief="Show all followed champions",
-#              description="Show a list of all champions that Zoe Bot will notify a Discord User for when one or "
-#                          "more champs are being played in a professional game. Remove a champion from this list "
-#                          "with the command '~track <champion_name>'.")
-# async def following(message):
-#     tracked_list = []
-#     user_id = message.author.id
-#     for champ_name in CHAMP_DICT.values():
-#         champion = Query()
-#         query_results = db.get(champion['champion_name'] == champ_name)
-#         if query_results is not None:
-#             user_ids_list = query_results['user_ids']
-#             if user_id in user_ids_list:
-#                 tracked_list.append(champ_name)
-#     if len(tracked_list) != 0:
-#         await message.channel.send(f"Currently tracking professional matches for: {', '.join(tracked_list)}")
-#     else:
-#         await message.channel.send(f"You are currently not tracking live games for any champion.")
-#
-#
-# @bot.command
-# def test():
-#     const.check_tracked_skins()
-
 
 help_command = commands.DefaultHelpCommand(no_category='List of Zoe Bot Commands')
 intents = discord.Intents.default()
@@ -376,6 +127,12 @@ class BaseCommand(commands.Bot):
             champion.lower()
             await channel.send(get_zoe_error_message())
 
+        @matchup.error
+        async def matchup_error(ctx, error):
+            if isinstance(error, discord.ext.commands.errors.MissingRequiredArgument):
+                await ctx.send(
+                    "use '~matchup <champion>' to search for Zoe's matchup statistics against a champion!")
+
         @self.command(brief="Paddle Star Damage Calculator (WIP)",
                       description="Zoe Q damage calculator based on items, runes, and masteries")
         async def psdc(channel):
@@ -386,19 +143,13 @@ class BaseCommand(commands.Bot):
         async def rotation(c):
             try:
                 free_rotation = []
-                for champion_id in free_champion_ids['freeChampionIds']:
+                for champion_id in self.free_champ_ids['freeChampionIds']:
                     free_rotation.append(CHAMP_DICT[str(champion_id)])
                 free_rotation.sort()
                 free_rotation = ', '.join(free_rotation)
                 await c.channel.send("The champions in this week's free to play rotation are: " + free_rotation)
             except (Exception,):
                 await c.channel.send(get_zoe_error_message())
-
-        @matchup.error
-        async def matchup_error(ctx, error):
-            if isinstance(error, discord.ext.commands.errors.MissingRequiredArgument):
-                await ctx.send(
-                    "use '~matchup <champion>' to search for Zoe's matchup statistics against a champion!")
 
         @self.command(brief="Show champion skins on sale this week",
                       description="Show list of all champion skins on sale, which refreshes every Monday at 3 pm EST")
@@ -441,53 +192,18 @@ class BaseCommand(commands.Bot):
                 await message.channel.send(f"No longer tracking skin sales for {champion_name}.")
 
         @self.command(brief="Track a champion in professional play",
-                      description="Receive messages from Zoe Bot whenever the given champion is being played in a profes"
-                                  "sional game, or use the command again to stop receiving notifications from Zoe Bot.")
+                      description="Receive messages from Zoe Bot whenever the given champion is being played in a "
+                                  "professional game, or use the command again to stop receiving notifications from "
+                                  "Zoe Bot.")
         async def follow(message, *champion_name):
-            # format champion_name
-            champion_name = self.format_champion_name(' '.join(champion_name))
-            if not champion_name:
-                await message.channel.send(
-                    f"use '~follow <champion>' to be notified when a champion is being played in a professional match!")
-                return
-            # Query champion user id list
-            champion = Query()
-            user_id = message.author.id
-            champ_name_user_ids_dict = db.get(champion['champion_name'] == champion_name)
-            # I tried using a set but it broke whenever i called db.insert()
-            user_ids_list = [] if champ_name_user_ids_dict is None else champ_name_user_ids_dict['user_ids']
-            if champ_name_user_ids_dict is None:
-                user_ids_list.append(user_id)
-                db.insert({'champion_name': champion_name, 'user_ids': user_ids_list})
-                await message.channel.send(f"Now tracking live professional games for {champion_name}.")
-            elif user_id not in user_ids_list:
-                user_ids_list.append(user_id)
-                db.update({'user_ids': user_ids_list}, champion['champion_name'] == champion_name)
-                await message.channel.send(f"Now following live professional games for {champion_name}.")
-            else:
-                user_ids_list.remove(user_id)
-                db.update({'user_ids': user_ids_list}, champion['champion_name'] == champion_name)
-                await message.channel.send(f"No longer following live professional games for {champion_name}.")
+            await FollowProCommand.follow(message, self, *champion_name)
 
         @self.command(brief="Show all followed champions",
-                      description="Show a list of all champions that Zoe Bot will notify a Discord User for when one or "
-                                  "more champs are being played in a professional game. Remove a champion from this list "
-                                  "with the command '~track <champion_name>'.")
+                      description="Show a list of all champions that Zoe Bot will notify a Discord User for when one "
+                                  "or more champs are being played in a professional game. Remove a champion from "
+                                  "this list with the command '~track <champion_name>'.")
         async def following(message):
-            tracked_list = []
-            user_id = message.author.id
-            for champ_name in CHAMP_DICT.values():
-                champion = Query()
-                query_results = db.get(champion['champion_name'] == champ_name)
-                if query_results is not None:
-                    user_ids_list = query_results['user_ids']
-                    if user_id in user_ids_list:
-                        tracked_list.append(champ_name)
-            if len(tracked_list) != 0:
-                await message.channel.send(
-                    f"Currently tracking professional matches for: {', '.join(tracked_list)}")
-            else:
-                await message.channel.send(f"You are currently not tracking live games for any champion.")
+            await FollowProCommand.following(message, self)
 
         @self.command
         def test():
