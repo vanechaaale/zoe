bf4cc4e7d3ea3a94263ab31a914ed4e76905032a
Vanessa <vanessal2001@gmail.com>
2022-08-03 02:42:59 -0400

    4 x 4 champion name display for ~rotation

--- Commands/WeeklyRotationCommand.py
+++ Commands/WeeklyRotationCommand.py
@@ -11,17 +11,24 @@ async def rotation(message):
         for champion_id in free_champ_ids['freeChampionIds']:
             free_rotation.append(champ_dict[str(champion_id)])
         free_rotation.sort()
-        mid = int(len(free_rotation)/2)
-        free_list1 = free_rotation[0:mid]
-        free_list2 = free_rotation[mid:len(free_rotation)]
-        value = ""
-        for i in range(mid):
-            current_line = f"{free_list1[i]} | {free_list2[i]}\n"
-            value += current_line
         embed = discord.Embed(color=0xe8bffb)
+        # For 2 x 8
+        # mid = int(len(free_rotation)/2)
+        # free_list1 = free_rotation[0:mid]
+        # free_list2 = free_rotation[mid:len(free_rotation)]
+        # value = ""
+        # for i in range(mid):
+        #     current_line = f"{free_list1[i]} | {free_list2[i]}\n"
+        #     value += current_line
+        # embed.add_field(
+        #     name="Weekly Free Champion Rotation:",
+        #     value=value,
+        #     inline=False
+        # )
         embed.add_field(
             name="Weekly Free Champion Rotation:",
-            value=value,
+            value=f"{' | '.join(free_rotation[0:4])}\n{' | '.join(free_rotation[4:8])}\n\n"
+                  f"{' | '.join(free_rotation[8:12])}\n{' | '.join(free_rotation[12:16])}",
             inline=False
         )
         # Send collage of all champion splash arts
